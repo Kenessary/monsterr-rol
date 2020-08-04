7b08ec0aaa8687da648a72216ea2838da7116047
Kenessary <kenessaryz@gmail.com>
2020-08-05 01:34:03 +0500

    Updates

---
+++ precache-manifest.a20cfbcf1310ee63996c0447f98bb96e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7274996be2eec72eb4d25cabc44113",
+    "url": "/monsterr-rol/index.html"
+  },
+  {
+    "revision": "6217b44f0f8861432234",
+    "url": "/monsterr-rol/static/css/main.9467bbdd.chunk.css"
+  },
+  {
+    "revision": "b81419f85758868b9ced",
+    "url": "/monsterr-rol/static/js/2.62d78548.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterr-rol/static/js/2.62d78548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6217b44f0f8861432234",
+    "url": "/monsterr-rol/static/js/main.30ed28c5.chunk.js"
+  },
+  {
+    "revision": "a0a978ffb8ad3f65cf32",
+    "url": "/monsterr-rol/static/js/runtime-main.9ab36edc.js"
+  }
+]);
